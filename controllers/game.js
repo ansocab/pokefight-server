607772c7d358b40fbbc96fc9701e02c4b765ee2b
Ana Caballero <ana.caballero@gmx.de>
2020-12-24 16:18:47 +0100

    sort leaderboard db entries by number of defeated pokemon

--- controllers/game.js
+++ controllers/game.js
@@ -2,12 +2,10 @@ const { Game } = require("../models");
 
 exports.saveGameResult = async (req, res) => {
   const dbResponse = await Game.create(req.body);
-  //console.log(dbResponse);
-  //console.log(req.body);
   return res.status(200).json(dbResponse);
 };
 
 exports.getLeaderboard = async (req, res) => {
-  const games = await Game.find();
+  const games = await Game.find().sort({ defeated_pokemon: 1 });
   res.send(games);
 };
