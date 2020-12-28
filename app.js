c1e7dfcdcaeb11fb1a8486fa57ec1ca8aa747b8e
Ana Caballero <ana.caballero@gmx.de>
2020-12-28 09:59:14 +0100

    update cors

--- app.js
+++ app.js
@@ -11,19 +11,7 @@ const typeRouter = require("./routes/type");
 const searchRouter = require("./routes/search");
 
 var app = express();
-
-const whitelist = ["http://localhost:3000", "https://pokemonrps.netlify.app/"]
-const options = {
-  origin: function(origin, callback) {
-    if(whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-} 
-app.use(cors(options));
-
+app.use(cors());
 app.use(express.json());
 const { PORT = 4000 } = process.env;
 app.use(logger("dev"));
